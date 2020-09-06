4fa2b9a207c6302b7f81b95d919e6171c98ece27
Chayce Knaub <chknaub@yahoo.com>
2020-09-06 14:43:31 -0400

    all endpoints tested

--- src/posts/posts.spec.js
+++ src/posts/posts.spec.js
@@ -29,7 +29,7 @@ describe("Posts endpoints", function () {
     db.raw("TRUNCATE posts, users RESTART IDENTITY CASCADE")
   );
 
-  describe.only(`GET /api/posts`, () => {
+  describe(`GET /api/posts`, () => {
     context(`Given no posts `, () => {
       it(`responds with 200 and an array`, () => {
         return supertest(app).get("/api/posts").expect(200, []);
@@ -48,31 +48,69 @@ describe("Posts endpoints", function () {
       });
 
       it(`responds with 200 and test array from fixtures`, () => {
-        return supertest(app).get("/api/posts").expect(200, testPosts);
+        return supertest(app)
+          .get("/api/posts")
+          .expect(200)
+          .expect((res) => {
+            expect(res.body[0].content).to.eql(testPosts[0].content);
+          });
       });
     });
   });
 
   describe(`POST /api/posts`, () => {
-    const testUser = {
-      id: 3,
-    };
-    const testPost = {
-      user_id: 3,
-      content: "blah blah",
-    };
     context(`Given a POST request`, () => {
+      const testPosts = makePostsArray();
+      const testUsers = makeUsersArray();
+
+      beforeEach("insert users", () => {
+        return db.into("users").insert(testUsers);
+      });
+
+      before("clean the table", () =>
+        db.raw("TRUNCATE posts, users RESTART IDENTITY CASCADE")
+      );
+
+      afterEach("cleanup", () =>
+        db.raw("TRUNCATE posts, users RESTART IDENTITY CASCADE")
+      );
+
       it(`responds with 201 `, () => {
+        const newPost = {
+          user_id: 2,
+          content: "new post test",
+        };
         return supertest(app)
           .post("/api/posts")
-          .send(testPost)
+          .send(newPost)
           .expect(201)
           .expect((res) => {
-            expect(res.user_id).to.eql(testPost.user_id);
-            expect(res.content).to.eql(testPost.content);
-            expect(res.headers.location).to.eql(
-              `/api/posts/${res.body.user_id}`
-            );
+            expect(res.body.content).to.eql(newPost.content);
+          });
+      });
+    });
+  });
+
+  describe(`GET /api/posts/:id`, () => {
+    context(`Given a post with an id`, () => {
+      const testPosts = makePostsArray();
+      const testUsers = makeUsersArray();
+
+      beforeEach("insert users", () => {
+        return db.into("users").insert(testUsers);
+      });
+      beforeEach("insert posts", () => {
+        return db.into("posts").insert(testPosts);
+      });
+
+      it(`responds with 200 and specified post `, () => {
+        const id = 2;
+        const expectedPost = testPosts.filter((post) => post.user_id === 2);
+        return supertest(app)
+          .get(`/api/posts/${id}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body[0].content).to.eql(expectedPost[0].content);
           });
       });
     });
